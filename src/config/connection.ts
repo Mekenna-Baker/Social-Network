2b4e260cbe5eb2ee597c0ac42ad4ac67f337cd24
mbaker <mekenna.drabner@gmail.com>
2024-10-07 14:26:10 -0600

    created function within connection.ts to connect to mongodb. Defined mongodb_uri within .env

---
+++ src/config/connection.ts
@@ -0,0 +1,18 @@
+//import mongoose
+import mongoose from 'mongoose';
+
+//connect to mongodb
+const db = async (): Promise<typeof mongoose.connection> =>  {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB');
+        console.log('Database connected');
+        return mongoose.connection;
+    } catch (error) {
+        console.error('Database connection failed');
+        throw new Error('Database connection failed');
+    }
+}
+
+
+//export connection
+export default db;
